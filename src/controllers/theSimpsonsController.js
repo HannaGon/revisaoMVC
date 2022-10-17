c1a3217be916f1c252800ed50baaf5d166fe7429
Hanna <hanna.goncalves.pro@gmail.com>
2022-10-17 12:49:51 -0300

    revisao parte 2 - post

--- src/controllers/theSimpsonsController.js
+++ src/controllers/theSimpsonsController.js
@@ -5,6 +5,27 @@ const obterPersonagens = async (request,response) =>{
     response.status(200).send(personagens)
 }
 
+const cadastrarPersonagem = async (request,response)=>{
+    const personagens = await db("the-simpsons")
+    //nome e idade obrigatorios
+    const {
+        nome, idade, catchphrase
+    } = request.body
+    //regras de negocio
+    //não permitir cadastro em dobro
+    const nomeExiste = personagens.some(personagem => personagem.nome === nome)
+    if (nomeExiste === true){
+        return response.status(409). send({ message: `O nome ${nome} já existe`})
+    }
+    //construir o personagem
+    const novoPersonagem = {
+        id: personagens.length,nome, idade, catchphrase
+    }
+    personagens.push(novoPersonagem)
+    response.status(201).send(novoPersonagem)
+}
+
 module.exports = {
-    obterPersonagens
+    obterPersonagens,
+    cadastrarPersonagem
 }
--- src/routes/theSimpsonsRoutes.js
+++ src/routes/theSimpsonsRoutes.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/theSimpsonsController")
 const router = express.Router()
 //path, controller
 router.get("/the-simpsons/personagens", controller.obterPersonagens)
+router.post("/the-simpsons/personagens", controller.cadastrarPersonagem)
 
 module.exports = router
